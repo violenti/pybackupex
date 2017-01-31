4f0f119659194a0eb1552c08d3d15ff9aa0be885
David <rodavidfernandez@gmail.com>
2017-01-31 14:41:22 -0300

    add pybackupex

---
+++ README.md
@@ -0,0 +1,1 @@
+#pybackupex
---
+++ backup.py
@@ -0,0 +1,30 @@
+
+import os
+import mysql.connector
+import subprocess
+from mysql.connector import errorcode
+
+
+cnx = mysql.connector.connect (user='root', password='root',
+                               host='localhost',buffered=True)
+cursor = cnx.cursor()
+
+query = "show databases;"
+
+path="/datadrive/backup/"
+cursor.execute(query)
+response = cursor.fetchall()
+
+for db in response:
+    dir=os.path.join(path, db[0])
+    print dir
+    if os.path.exists(path + db[0]) == True :
+        subprocess.call(['innobackupex', '--user=root', '--password=root','--databases=db[0].*', dir,'--no-timestamp'])
+    else:
+         os.mkdir( path + db[0],0755 )
+         subprocess.call(['innobackupex', '--user=root', '--password=root','--databases=db[0].*', dir,'--no-timestamp'])
+    print db[0]
+
+
+cursor.close()
+cnx.close()
