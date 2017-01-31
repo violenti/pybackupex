63f58764213b495ef3a977b485131aff1873a1ac
David <rodavidfernandez@gmail.com>
2017-01-31 19:04:33 -0300

    add variable user and password

--- backup.py
+++ backup.py
@@ -4,8 +4,9 @@ import mysql.connector
 import subprocess
 from mysql.connector import errorcode
 
-
-cnx = mysql.connector.connect (user='root', password='root',
+USER='root'
+PASS='root'
+cnx = mysql.connector.connect (user=USER, password=PASS,
                                host='localhost',buffered=True)
 cursor = cnx.cursor()
 
@@ -19,11 +20,11 @@ for db in response:
     dir=os.path.join(path, db[0])
     print dir
     if os.path.exists(path + db[0]) == True :
-        subprocess.call(['innobackupex', '--user=root', '--password=root','--databases="db[0]" ' , dir,'--no-timestamp'])
+        subprocess.call(['innobackupex', '--user=root','--password=root','--databases="db[0]" ' , dir,'--no-timestamp'])
         subprocess.call(['innobackupex','--apply-log','--export', dir])
     else:
-         os.mkdir( path + db[0],0755 )
-         subprocess.call(['innobackupex', '--user=root', '--password=root','--databases="db[0]" ', dir,'--no-timestamp'])
+         os.mkdir( path + db[0],0777 )
+         subprocess.call(['innobackupex', '--user=root','--password=root','--databases="db[0]" ', dir,'--no-timestamp'])
          subprocess.call(['innobackupex','--apply-log','--export', dir])
 
     print db[0]
